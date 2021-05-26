f259e6805f851f7b5117100dd39677c48b973f6f
lanboys <lan_bing2013@163.com>
2021-05-26 21:07:47 +0800

    配置json格式化工具objectMapper

--- rpc-example/rpc-example-consumer/src/main/java/com/mini/rpc/example/consumer/RpcConsumerApplication.java
+++ rpc-example/rpc-example-consumer/src/main/java/com/mini/rpc/example/consumer/RpcConsumerApplication.java
@@ -1,9 +1,12 @@
 package com.mini.rpc.example.consumer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mini.rpc.consumer.RpcConsumerAutoConfiguration;
+import com.mini.rpc.example.consumer.utils.JsonUtil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
@@ -12,5 +15,10 @@ public class RpcConsumerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(RpcConsumerApplication.class, args);
+  }
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    return JsonUtil.createObjectMapper();
   }
 }
---
+++ rpc-example/rpc-example-consumer/src/main/java/com/mini/rpc/example/consumer/utils/JsonUtil.java
@@ -0,0 +1,78 @@
+package com.mini.rpc.example.consumer.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class JsonUtil {
+
+  private static final JsonUtil ourInstance = new JsonUtil();
+
+  public static JsonUtil getInstance() {
+    return ourInstance;
+  }
+
+  public static ObjectMapper createObjectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(createJavaTimeModule());
+    return objectMapper;
+  }
+
+  public static JavaTimeModule createJavaTimeModule() {
+    JavaTimeModule javaTimeModule = new JavaTimeModule();
+    javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+    javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
+    javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+    javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
+    return javaTimeModule;
+  }
+
+  private ObjectMapper objectMapper;
+
+  private JsonUtil() {
+    objectMapper = createObjectMapper();
+  }
+
+  //public ObjectMapper objectMapper() {
+  //    return objectMapper;
+  //}
+
+  public <T> T toObject(InputStream src, Class<T> valueType) {
+    try {
+      return objectMapper.readValue(src, valueType);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public <T> T toObject(String src, Class<T> valueType) {
+    try {
+      return objectMapper.readValue(src, valueType);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public String toString(Object value) {
+    try {
+      return objectMapper.writeValueAsString(value);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
